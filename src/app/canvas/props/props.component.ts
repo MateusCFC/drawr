5241551807209dfb35cf08b4a8b9e032fb8a3ecb
Gilmar Andrade <gilmar.andrade@imd.ufrn.br>
2018-04-25 10:18:47 -0300

    seletor básico de cores RGB

--- src/app/canvas/props/props.component.ts
+++ src/app/canvas/props/props.component.ts
@@ -17,6 +17,11 @@ export class PropsComponent implements OnInit {
   rotation: number;
   transparency: number;
 
+  //auxiliares para definição da cor de preenchimento
+  red: number;
+  green: number;
+  blue: number;
+
   fill: string | Gradient | Pattern;
   stroke: string | Gradient;
   lineWidth: number;
@@ -37,6 +42,11 @@ export class PropsComponent implements OnInit {
         this.rotation = this.editorService.selectedShape.rotation;
         this.transparency = this.editorService.selectedShape.style.transparency * 100;
 
+        let rgb = this.hexToRgb(this.editorService.selectedShape.properties.style.fill);
+        this.red = rgb.r;
+        this.green = rgb.g;
+        this.blue = rgb.b;
+
         this.fill = this.editorService.selectedShape.properties.style.fill;
 
         this.lineWidth = this.editorService.selectedShape.properties.style.lineWidth;
@@ -55,12 +65,45 @@ export class PropsComponent implements OnInit {
     this.editorService.selectedShape.rotation = this.rotation;
     this.editorService.selectedShape.style.transparency = this.transparency / 100;
 
+    this.fill = this.rgbToHex(this.red, this.green, this.blue);
     this.editorService.selectedShape.properties.style.fill = this.fill;
+
     this.editorService.selectedShape.properties.style.lineWidth = this.lineWidth;
     this.editorService.selectedShape.properties.style.stroke = this.stroke;
 
     this.canvas.figure.refresh();
     console.log("updated shape");
+  }
+
+  /**
+   * Converte valores RGB em seu correspondente Hexadecimal
+   * https://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
+   * @param r
+   * @param g
+   * @param b
+   */
+  rgbToHex(r, g, b) {
+    return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+  }
+
+  /**
+   * Converte um valor em Hexadecimal no seu objeto correspondente RGB
+   * https://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
+   * @param hex
+   */
+  hexToRgb(hex) {
+    // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+    let shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+    hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+        return r + r + g + g + b + b;
+    });
+
+    let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+    } : null;
   }
 
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,7 +3,7 @@ import { CommonModule, } from '@angular/common';
 import { FlexLayoutModule, } from '@angular/flex-layout';
 import {
   MatButtonModule, MatCheckboxModule, MatToolbarModule, MatIconModule,
-  MatButtonToggleModule, MatSidenavModule, MatFormFieldModule, MatInputModule
+  MatButtonToggleModule, MatSidenavModule, MatFormFieldModule, MatInputModule, MatSliderModule
 } from '@angular/material';
 
 const LAYOUT_MODULES: any[] = [
@@ -12,7 +12,7 @@ const LAYOUT_MODULES: any[] = [
 
 const MATERIAL_MODULES: any[] = [
   MatButtonModule, MatCheckboxModule, MatToolbarModule, MatIconModule,
-  MatButtonToggleModule, MatSidenavModule, MatFormFieldModule, MatInputModule
+  MatButtonToggleModule, MatSidenavModule, MatFormFieldModule, MatInputModule, MatSliderModule
 ];
 
 /**
